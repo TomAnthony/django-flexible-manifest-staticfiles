0fa7534dcec1de62cad8e59cbcf26b6aef18c49c
Tom Anthony <git@tomanthony.co.uk>
2019-11-27 14:54:02 +0000

    Update to correctly filter references in other files (e.g. import() or url() statements in a CSS file)

--- django_flexible_manifest_staticfiles/storages.py
+++ django_flexible_manifest_staticfiles/storages.py
@@ -23,3 +23,24 @@ class FlexibleManifestStaticFilesStorage(ManifestStaticFilesStorage):
                     filtered_paths[name] = val
         
         return super()._post_process(filtered_paths, adjustable_paths, hashed_files)
+
+
+    def url_converter(self, name, hashed_files, template=None):
+
+        super_converter = super().url_converter(name, hashed_files, template)
+
+        def converter(matchobj):
+            matched, url = matchobj.groups()
+
+            should_convert = False
+
+            if any(re.search(wp, url) for wp in self.whitelisted_patterns):
+                if not any(re.search(wp, url) for wp in self.blacklisted_patterns):
+                    should_convert = True
+
+            if not should_convert:
+                return matched
+
+            return super_converter(matchobj)
+
+        return converter
